ecffd8f478bc570c9b9bff63c5ed2aa5dc765095
shailesh <shailesh.kumarcs@gmail.com>
2018-03-14 23:03:11 +0530

    Resovle markdown_duex  problem

--- posts/models.py
+++ posts/models.py
@@ -5,8 +5,11 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils import timezone
-
+from django.utils.safestring import mark_safe
 from django.utils.text import  slugify
+
+from markdown_deux import markdown
+
 
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
@@ -56,6 +59,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
+
+    def get_markdown(self):
+        content = self.content
+        return mark_safe(markdown(content))
 
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
